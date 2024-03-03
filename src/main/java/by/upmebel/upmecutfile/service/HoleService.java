b6914c4885221a072802d15528491940b95d3d4c
Vasili Bandarenka <darken123@inbox.ru>
2024-03-03 17:30:24 +0300

    Добавлена проверка есть ли деталь с таким номером
    При сохранении отверстия в HoleService
    проверяется есть ли деталь с таким номером.

--- src/main/java/by/upmebel/upmecutfile/service/HoleService.java
+++ src/main/java/by/upmebel/upmecutfile/service/HoleService.java
@@ -3,14 +3,21 @@ package by.upmebel.upmecutfile.service;
 
 import by.upmebel.upmecutfile.model.Hole;
 import by.upmebel.upmecutfile.repository.HoleRepository;
+import by.upmebel.upmecutfile.repository.PartRepository;
+import jakarta.persistence.EntityExistsException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class HoleService {
     private final HoleRepository holeRepository;
+    private final PartRepository partRepository;
+
 
     public List<Hole> getAll() {
         return holeRepository.findAll();
@@ -21,7 +28,9 @@ public class HoleService {
     }
 
     public Hole save(Hole hole) {
-        //TODO проверить что есть деталь к которой принадлежит отверстие
+        if (!partRepository.existsById(hole.getPart().getId())) {
+            throw new EntityExistsException("Детали с Id " + hole.getPart().getId()  + " нет в базе данных");
+        }
         return holeRepository.save(hole);
     }
 }
