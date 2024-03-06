5dc59f8c466c080293710a11c343125c00ae65dd
Vasili Bandarenka <darken123@inbox.ru>
2024-03-06 14:41:01 +0300

    Рефакторинг кода
    Переписал PatternConverter с использованием функционального интерфейса

--- src/main/java/by/upmebel/upmecutfile/utils/PatternConverter.java
+++ src/main/java/by/upmebel/upmecutfile/utils/PatternConverter.java
@@ -1,50 +1,41 @@
 package by.upmebel.upmecutfile.utils;
 
 import dto.Pattern;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
 public class PatternConverter {
 
+    @FunctionalInterface
+    interface Operation {
+        double apply(double variable, double value);
+    }
+
     public Double convert(List<Pattern> patterns, Double L, Double B, Double H) {
         double result = 0.0;
 
+        Map<String, Operation> operations = Map.of(
+                "+", (variable, value) -> variable + value,
+                "-", (variable, value) -> variable - value,
+                "*", (variable, value) -> variable * value,
+                "/", (variable, value) -> variable / value
+        );
+
         for (Pattern pattern : patterns) {
+            Operation operation = operations.getOrDefault(pattern.getOperator(), (variable, value) -> variable);
             switch (pattern.getVariable()) {
-                case "L" -> {
-                    switch (pattern.getOperator()) {
-                        case "+" -> result += L + pattern.getValue();
-                        case "-" -> result += L - pattern.getValue();
-                        case "*" -> result += L * pattern.getValue();
-                        case "/" -> result += L / pattern.getValue();
-                    }
-                }
-                case "B" -> {
-                    switch (pattern.getOperator()) {
-                        case "+" -> result += B + pattern.getValue();
-                        case "-" -> result += B - pattern.getValue();
-                        case "*" -> result += B * pattern.getValue();
-                        case "/" -> result += B / pattern.getValue();
-                    }
-                }
-                case "H" -> {
-                    switch (pattern.getOperator()) {
-                        case "+" -> result += H + pattern.getValue();
-                        case "-" -> result += H - pattern.getValue();
-                        case "*" -> result += H * pattern.getValue();
-                        case "/" -> result += H / pattern.getValue();
-                    }
-                }
+                case "L" -> result += operation.apply(L, pattern.getValue());
+                case "B" -> result += operation.apply(B, pattern.getValue());
+                case "H" -> result += operation.apply(H, pattern.getValue());
                 case "" -> result += pattern.getValue();
             }
         }
-        return result;
 
+        return result;
     }
 }
